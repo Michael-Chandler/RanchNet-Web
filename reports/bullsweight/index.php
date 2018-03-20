cab24874a6aadcab3de002237a89380e98d8bafa
adriane34 <paolo.aytona@gmail.com>
2018-03-20 02:50:11 +0000

    Fix on reports nav, Fix on bulls weight report

--- reports/bullsweight/index.php
+++ reports/bullsweight/index.php
@@ -1,6 +1,5 @@
 <?php
 include_once('../auth.php');
-include_once('process.php');
 ?>
 
 <!DOCTYPE html>
@@ -25,6 +24,153 @@ include_once('process.php');
 </head>
 
 <body class="fixed-nav sticky-footer bg-dark" id="page-top">
+
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+    <a class="navbar-brand" href="../cattlemanager">RanchNet</a>
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
+            <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Cattle Manager">
+            <a class="nav-link" href="../cattlemanager">
+                <i class="fa fa-fw fa-dashboard"></i>
+                <span class="nav-link-text">Cattle Manager</span>
+            </a>
+            </li>
+            <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Pasture Manager">
+            <a class="nav-link" href="../pasturemanager">
+                <i class="fa fa-fw fa-map"></i>
+                <span class="nav-link-text">Pasture Manager</span>
+            </a>
+            </li>
+            <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Reports">
+            <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseComponents" data-parent="#exampleAccordion">
+                <i class="fa fa-fw fa-sitemap"></i>
+                <span class="nav-link-text">Reports</span>
+            </a>
+            <ul class="sidenav-second-level collapse" id="collapseComponents">
+                <li>
+                <a href="reports/pair">Pair Up</a>
+                </li>
+                <li>
+                <a href="reports/bullsweight">Weight of all Bulls</a>
+                </li>
+            </ul>
+            </li>
+            <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Settings">
+            <a class="nav-link" href="../settings">
+                <i class="fa fa-fw fa-wrench"></i>
+                <span class="nav-link-text">Settings</span>
+            </a>
+            </li>
+        </ul>
+        <ul class="navbar-nav sidenav-toggler">
+            <li class="nav-item">
+            <a class="nav-link text-center" id="sidenavToggler">
+                <i class="fa fa-fw fa-angle-left"></i>
+            </a>
+            </li>
+        </ul>
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle mr-lg-2" id="messagesDropdown" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fa fa-fw fa-envelope"></i>
+                <span class="d-lg-none">Messages
+                    <span class="badge badge-pill badge-primary">12 New</span>
+                </span>
+                <span class="indicator text-primary d-none d-lg-block">
+                    <i class="fa fa-fw fa-circle"></i>
+                </span>
+            </a>
+            <div class="dropdown-menu" aria-labelledby="messagesDropdown">
+                <h6 class="dropdown-header">New Messages:</h6>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">
+                    <strong>David Miller</strong>
+                    <span class="small float-right text-muted">11:21 AM</span>
+                    <div class="dropdown-message small">Hey there! This new version of SB Admin is pretty awesome! These messages clip off when they reach the end of the box so they don't overflow over to the sides!</div>
+                </a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">
+                    <strong>Jane Smith</strong>
+                    <span class="small float-right text-muted">11:21 AM</span>
+                    <div class="dropdown-message small">I was wondering if you could meet for an appointment at 3:00 instead of 4:00. Thanks!</div>
+                </a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">
+                    <strong>John Doe</strong>
+                    <span class="small float-right text-muted">11:21 AM</span>
+                    <div class="dropdown-message small">I've sent the final files over to you for review. When you're able to sign off of them let me know and we can discuss distribution.</div>
+                </a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item small" href="#">View all messages</a>
+            </div>
+            </li>
+            <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle mr-lg-2" id="alertsDropdown" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fa fa-fw fa-bell"></i>
+                <span class="d-lg-none">Alerts
+                    <span class="badge badge-pill badge-warning">6 New</span>
+                </span>
+                <span class="indicator text-warning d-none d-lg-block">
+                    <i class="fa fa-fw fa-circle"></i>
+                </span>
+            </a>
+            <div class="dropdown-menu" aria-labelledby="alertsDropdown">
+                <h6 class="dropdown-header">New Alerts:</h6>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">
+                    <span class="text-success">
+                        <strong>
+                            <i class="fa fa-long-arrow-up fa-fw"></i>Status Update</strong>
+                    </span>
+                    <span class="small float-right text-muted">11:21 AM</span>
+                    <div class="dropdown-message small">This is an automated server response message. All systems are online.</div>
+                </a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">
+                    <span class="text-danger">
+                        <strong>
+                            <i class="fa fa-long-arrow-down fa-fw"></i>Status Update</strong>
+                    </span>
+                    <span class="small float-right text-muted">11:21 AM</span>
+                    <div class="dropdown-message small">This is an automated server response message. All systems are online.</div>
+                </a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">
+                    <span class="text-success">
+                        <strong>
+                            <i class="fa fa-long-arrow-up fa-fw"></i>Status Update</strong>
+                    </span>
+                    <span class="small float-right text-muted">11:21 AM</span>
+                    <div class="dropdown-message small">This is an automated server response message. All systems are online.</div>
+                </a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item small" href="#">View all alerts</a>
+            </div>
+            </li>
+            <li class="nav-item">
+            <!-- <form class="form-inline my-2 my-lg-0 mr-lg-2">
+                <div class="input-group">
+                    <input class="form-control" type="text" placeholder="Search for...">
+                    <span class="input-group-append">
+                        <button class="btn btn-primary" type="button">
+                            <i class="fa fa-search"></i>
+                        </button>
+                    </span>
+                </div>
+            </form> -->
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" data-toggle="modal" data-target="#exampleModal">
+                <i class="fa fa-fw fa-sign-out"></i>Logout</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
 	<div class="content-wrapper">
 		<div class="container-fluid">
 			
@@ -35,6 +181,35 @@ include_once('process.php');
 				</div>
 				<div class="card-body">
 					<canvas id="barChart" width="100%" height="30"></canvas>
+					
+<!-- Gets all male cattle tags and weight for each-->
+<?php
+// init vars
+$cattleArray = array();
+$weightArray = array();
+
+// set up URL
+$URL = API_URL
+	."cattle"
+	."?token=".API_SECRET
+	."&cattleSex=M"
+	."&userId=".$_SESSION["userId"];
+
+// using cURL
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL, $URL);
+$result = curl_exec($ch);
+curl_close($ch);
+
+// get php object
+$obj = json_decode($result);
+foreach($obj as $line) {
+	array_push($cattleArray, $line->cattleTag);
+	array_push($weightArray, $line->cattleWeight);
+}
+?>
 				</div>
 			</div>
 			
@@ -135,3 +310,5 @@ echo "<a class=\"btn btn-primary\" href=".WEB_URL."/logout>Logout</a>";
 </body>
 
 </html>
+
+
