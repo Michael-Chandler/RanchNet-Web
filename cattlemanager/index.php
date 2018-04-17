8ebccaa6531500f24f903ed296fe124c0c312e5c
Everene <mikee.chandler@yahoo.com>
2018-04-17 04:04:23 +0000

    Fixed pastureId resolution when adding/editing cattle.

--- cattlemanager/index.php
+++ cattlemanager/index.php
@@ -14,6 +14,23 @@ curl_setopt($rch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($rch, CURLOPT_URL, $rURL);
 $report = curl_exec($rch);
 curl_close($rch);
+
+// get all pastures to list
+$URL = API_URL
+    ."pastures"
+    ."?token=".API_SECRET
+    ."&userId=".$_SESSION["userId"];
+
+// using cURL
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL, $URL);
+$result = curl_exec($ch);
+curl_close($ch);
+
+// get php object
+$pastures = json_decode($result);
 // MORE at the nav menu
 
 // get record to be updated -> fills the form and changes the Add button to updated
@@ -198,95 +215,6 @@ if(isset($_GET["more"])) {
             </li>
         </ul>
         <ul class="navbar-nav ml-auto">
-            <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle mr-lg-2" id="messagesDropdown" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fa fa-fw fa-envelope"></i>
-                <span class="d-lg-none">Messages
-                    <span class="badge badge-pill badge-primary">12 New</span>
-                </span>
-                <span class="indicator text-primary d-none d-lg-block">
-                    <i class="fa fa-fw fa-circle"></i>
-                </span>
-            </a>
-            <div class="dropdown-menu" aria-labelledby="messagesDropdown">
-                <h6 class="dropdown-header">New Messages:</h6>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">
-                    <strong>David Miller</strong>
-                    <span class="small float-right text-muted">11:21 AM</span>
-                    <div class="dropdown-message small">Hey there! This new version of SB Admin is pretty awesome! These messages clip off when they reach the end of the box so they don't overflow over to the sides!</div>
-                </a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">
-                    <strong>Jane Smith</strong>
-                    <span class="small float-right text-muted">11:21 AM</span>
-                    <div class="dropdown-message small">I was wondering if you could meet for an appointment at 3:00 instead of 4:00. Thanks!</div>
-                </a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">
-                    <strong>John Doe</strong>
-                    <span class="small float-right text-muted">11:21 AM</span>
-                    <div class="dropdown-message small">I've sent the final files over to you for review. When you're able to sign off of them let me know and we can discuss distribution.</div>
-                </a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item small" href="#">View all messages</a>
-            </div>
-            </li>
-            <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle mr-lg-2" id="alertsDropdown" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fa fa-fw fa-bell"></i>
-                <span class="d-lg-none">Alerts
-                    <span class="badge badge-pill badge-warning">6 New</span>
-                </span>
-                <span class="indicator text-warning d-none d-lg-block">
-                    <i class="fa fa-fw fa-circle"></i>
-                </span>
-            </a>
-            <div class="dropdown-menu" aria-labelledby="alertsDropdown">
-                <h6 class="dropdown-header">New Alerts:</h6>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">
-                    <span class="text-success">
-                        <strong>
-                            <i class="fa fa-long-arrow-up fa-fw"></i>Status Update</strong>
-                    </span>
-                    <span class="small float-right text-muted">11:21 AM</span>
-                    <div class="dropdown-message small">This is an automated server response message. All systems are online.</div>
-                </a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">
-                    <span class="text-danger">
-                        <strong>
-                            <i class="fa fa-long-arrow-down fa-fw"></i>Status Update</strong>
-                    </span>
-                    <span class="small float-right text-muted">11:21 AM</span>
-                    <div class="dropdown-message small">This is an automated server response message. All systems are online.</div>
-                </a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">
-                    <span class="text-success">
-                        <strong>
-                            <i class="fa fa-long-arrow-up fa-fw"></i>Status Update</strong>
-                    </span>
-                    <span class="small float-right text-muted">11:21 AM</span>
-                    <div class="dropdown-message small">This is an automated server response message. All systems are online.</div>
-                </a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item small" href="#">View all alerts</a>
-            </div>
-            </li>
-            <li class="nav-item">
-            <!-- <form class="form-inline my-2 my-lg-0 mr-lg-2">
-                <div class="input-group">
-                    <input class="form-control" type="text" placeholder="Search for...">
-                    <span class="input-group-append">
-                        <button class="btn btn-primary" type="button">
-                            <i class="fa fa-search"></i>
-                        </button>
-                    </span>
-                </div>
-            </form> -->
-            </li>
             <li class="nav-item">
             <a class="nav-link" data-toggle="modal" data-target="#exampleModal">
                 <i class="fa fa-fw fa-sign-out"></i>Logout</a>
@@ -305,163 +233,7 @@ if(isset($_GET["more"])) {
                     <div class="card-body">
 					
                         <!-- Input Form -->
-                        <button type="button" class="btn btn-info" data-toggle="modal" data-target="#myModal">Add Cattle</button>
-                        <div id="myModal" class="modal fade" role="dialog">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-								
-								<?php if($edit_state): ?>
-									<h2>Edit Cattle</h2>
-								<?php else: ?>
-									<h2>Add Cattle</h2>
-								<?php endif ?>
-								
-									<!-- Cattle Input Form -->
-									<form method="POST" action="process.php">
-										<input type ="hidden" name="cattleId" value="<?php echo $cattleId; ?>">
-										<label type="text" class="form-control-label"><strong>User ID: <?php echo $_SESSION["userId"]; ?></strong></label>
-										<br>
-										<label type="text" class="form-control-label"><strong>Measurement System: 
-										<?php 
-										if(isset($_SESSION["measure"])) {
-											echo $_SESSION["measure"];
-										} else {
-											echo "English (default)";
-										}
-										?>
-										</strong></label>
-										<br>
-										<!-- Name, Sex, Animal Type input -->
-										<div class="form-group">
-											<div class="form-row">
-												<div class="col-md-4">
-													<label for="cattleName" class="form-control-label"><strong>Name: </strong></label>
-													<input type="text" class="form-control" id="cattleName" name="cattleName" maxlength="64" value="<?php echo $cattleName; ?>" placeholder="Enter Cattle Name here" required>
-												</div>
-												<div class="col-md-4">
-													<label for="cattleSex" class="form-control-label"><strong>Sex: </strong></label>
-													<select class="form-control" id="cattleSex" name="cattleSex">
-													<?php if($cattleSex == "M"): ?>
-														<option value="M" selected>M</option>
-														<option value="F">F</option>
-													<?php else: ?>
-														<option value="M">M</option>
-														<option value="F" selected>F</option>
-													<?php endif ?>
-													</select>
-												</div>
-												<div class="col-md-4">
-													<label for="cattleAnimalType" class="form-control-label"><strong>Animal Type: </strong></label>
-													<input type="text" class="form-control" id="cattleAnimalType" name="cattleAnimalType" maxlength="128" value="<?php echo $cattleAnimalType; ?>" placeholder="Enter Animal Type here">
-												</div>
-											</div>
-										</div>
-										<!-- Tag, Reg Num, Elec ID input -->
-										<div class="form-group">
-											<div class="form-row">
-												<div class="col-md-4">
-													<label for="cattleTag" class="form-control-label"><strong>Tag: </strong></label>
-													<input type="text" class="form-control" id="cattleTag" name="cattleTag" maxlength="128" value="<?php echo $cattleTag; ?>" placeholder="Enter Cattle Tag here"> 
-												</div>
-												<div class="col-md-4">
-													<label for="cattleRegisteredNumber" class="form-control-label"><strong>Registered Number: </strong></label>
-													<input type="text" class="form-control" id="cattleRegisteredNumber" name="cattleRegisteredNumber" maxlength="128" value="<?php echo $cattleRegisteredNumber; ?>" placeholder="Enter Cattle Registered Number here">
-												</div>
-												<div class="col-md-4">
-													<label for="cattleElectronicId" class="form-control-label"><strong>Electronic ID: </strong></label>
-													<input type="text" class="form-control" id="cattleElectronicId" name="cattleElectronicId" maxlength="128" value="<?php echo $cattleElectronicId; ?>" placeholder="Enter Cattle Electronic ID here">
-												</div>
-											</div>
-										</div>
-										<!-- Sire input -->
-										<div class="form-group">
-											<div class="form-row">
-												<div class="col-md-4">
-													<label for="cattleSireName" class="form-control-label"><strong>Sire Name: </strong></label>
-													<input type="text" class="form-control" id="cattleSireName" name="cattleSireName" maxlength="64" value="<?php echo $cattleSireName; ?>" placeholder="Enter Sire Name here">
-												</div>
-												<div class="col-md-8">
-													<label for="cattleSireRegisteredNumber" class="form-control-label"><strong>Sire Registered Number: </strong></label>
-													<input type="text" class="form-control" id="cattleSireRegisteredNumber" name="cattleSireRegisteredNumber" maxlength="128" value="<?php echo $cattleSireRegisteredNumber; ?>" placeholder="Enter Sire Registered Number here">
-												</div>
-											</div>
-										</div>
-										<!-- Dam input -->
-										<div class="form-group">
-											<div class="form-row">
-												<div class="col-md-4">
-													<label for="cattleDamName" class="form-control-label"><strong>Dam Name: </strong></label>
-													<input type="text" class="form-control" id="cattleDamName" name="cattleDamName" maxlength="64" value="<?php echo $cattleDamName; ?>" placeholder="Enter Dam Name here">
-												</div>
-												<div class="col-md-8">
-													<label for="cattleDamRegisteredNumber" class="form-control-label"><strong>Dam Registered Number: </strong></label>
-													<input type="text" class="form-control" id="cattleDamRegisteredNumber" name="cattleDamRegisteredNumber" maxlength="128" value="<?php echo $cattleDamRegisteredNumber; ?>" placeholder="Enter Dam Registered Number here">
-												</div>
-											</div>
-										</div>
-										<!-- DOB, Contraception, Breeder, Pregnant input -->
-										<div class="form-group">
-											<div class="form-row">
-												<div class="col-md-3">
-													<label for="cattleDateOfBirth" class="form-control-label"><strong>Date of Birth: </strong></label>
-													<input type="date" class="form-control" id="cattleDateOfBirth" name="cattleDateOfBirth" value="<?php echo date($cattleDateOfBirth); ?>">
-												</div>
-												<div class="col-md-3">
-													<label for="cattleContraception" class="form-control-label"><strong>Contraception: </strong></label>
-													<input type="text" class="form-control" id="cattleContraception" name="cattleContraception" maxlength="64" value="<?php echo $cattleContraception; ?>" placeholder="Enter Cattle Contraception here">
-												</div>
-												<div class="col-md-3">
-													<label for="cattleBreeder" class="form-control-label"><strong>Breeder: </strong></label>
-													<input type="text" class="form-control" id="cattleBreeder" name="cattleBreeder" maxlength="64" value="<?php echo $cattleBreeder; ?>" placeholder="Enter Cattle Breeder here">
-												</div>
-												<div class="col-md-3">
-													<label for="cattlePregnant" class="form-control-label"><strong>Pregnant: </strong></label>
-													<select class="form-control" id="cattlePregnant" name="cattlePregnant">
-													<?php if($cattlePregnant == 0): ?>
-														<option value="0" selected>No</option>
-														<option value="1">Yes</option>
-													<?php else: ?>
-														<option value="0">No</option>
-														<option value="1" selected>Yes</option>
-													<?php endif ?>
-													</select>
-												</div>
-											</div>
-										</div>
-										<!-- Height, Weight, Pasture input -->
-                                        <div class="form-group">
-											<div class="form-row">
-												<div class="col-md-4">
-													<label for="cattleHeight" class="form-control-label"><strong>Height: </strong></label>
-													<input type="text" class="form-control" id="cattleHeight" name="cattleHeight" maxlength="64" value="<?php echo $cattleHeight; ?>" placeholder="Enter Cattle Height here">
-												</div>
-												<div class="col-md-4">
-													<label for="cattleWeight" class="form-control-label"><strong>Weight: </strong></label>
-													<input type="text" class="form-control" id="cattleWeight" name="cattleWeight" maxlength="64" value="<?php echo $cattleWeight; ?>" placeholder="Enter Cattle Weight here">
-												</div>
-												<div class="col-md-4">
-													<label for="pastureId" class="form-control-label"><strong>Pasture ID: </strong></label>
-													<input type="text" class="form-control" id="pastureId" name="pastureId" maxlength="11" 
-														value="<?php 
-																if(!isset($pastureId)) {
-																	echo 1;
-																} else {
-																	echo $pastureId;
-																}?>" placeholder="Enter Pasture ID here">
-												</div>
-											</div>
-										</div>
-                                        <!-- From buttons -->
-										<?php if($edit_state): ?>
-											<button type="submit" class="form-control" id="update" name="update" class="btn">Update Cattle</button>
-										<?php else: ?>
-											<button type="submit" class="form-control" id="add" name="add" class="btn">Add Cattle</button>
-										<?php endif ?>
-                                        <a href="/cattlemanager" id="cancel" name="cancel" class="form-control btn">Cancel</a>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
+                        <?php include_once('../blocks/cattlemodal.php'); ?>
 						
                         <div class="table-responsive">
 						
